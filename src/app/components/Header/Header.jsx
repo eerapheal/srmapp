2f8b808485586b0f0bbe360c028c8a7e482ddeee
Ekpenisi Erue Raphael <ekpenisiraphael@gmail.com>
2023-11-06 08:16:18 +0530

    Nav bar set up

--- src/app/components/Header/Header.jsx
+++ src/app/components/Header/Header.jsx
@@ -1,11 +1,26 @@
+import Image from "next/image";
 import styles from "./Header.module.css";
-
+import Link from "next/link";
 import React from 'react'
 
 const Header = () => {
   return (
     <div className={styles.container}>
-      Header
+      <div className="styles.logo">
+        <h1>SRM</h1>
+      </div>
+      <div className={styles.socical}>
+       <Image src="/facebook.png" alt="Facebook" width={24} height={24} />
+       <Image src="/youtube.png" alt="Youtube" width={24} height={24} />
+       <Image src="/instagram.png" alt="Instagram" width={24} height={24} />
+       <Image src="/tiktok.png" alt="Tiktok" width={24} height={24} />
+       <Image src="/moon.png" alt="Twitter" width={24} height={24} />
+      </div>
+      <div className={styles.links}>
+        <Link href="/" >Home</Link>
+        <Link href="/" >About</Link>
+        <Link href="/" >Contact</Link>
+      </div>
     </div>
   )
 }
