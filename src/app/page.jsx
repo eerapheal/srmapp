bfe8268c1a1d9122335f656fff9e1c4c60d21f12
Ekpenisi Erue Raphael <ekpenisiraphael@gmail.com>
2023-11-06 06:36:31 +0530

    basic set up

---
+++ src/app/page.jsx
@@ -0,0 +1,13 @@
+import styles from "./homepage.module.css";
+
+export default function Home() {
+  return <div>
+    <h1>Sant Ralph Ragazine</h1>
+    <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus quod exercitationem quidem perferendis quae facere provident ducimus quos. Provident mollitia quo cupiditate natus suscipit perspiciatis rem repellat ratione aperiam velit.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus quod exercitationem quidem perferendis quae facere provident ducimus quos. Provident mollitia quo cupiditate natus suscipit perspiciatis rem repellat ratione aperiam velit.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus quod exercitationem quidem perferendis quae facere provident ducimus quos. Provident mollitia quo cupiditate natus suscipit perspiciatis rem repellat ratione aperiam velit.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus quod exercitationem quidem perferendis quae facere provident ducimus quos. Provident mollitia quo cupiditate natus suscipit perspiciatis rem repellat ratione aperiam velit.
+    </p>
+  </div>;
+}
