fdd31cd38b330b0134ba7d9ca8ca134146a5baca
Ekpenisi Erue Raphael <ekpenisiraphael@gmail.com>
2023-11-06 17:11:39 +0530

    Dark mode set up

--- src/app/components/themeToggle/ThemeToggle.jsx
+++ src/app/components/themeToggle/ThemeToggle.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import styles from "./themeToggle.module.css";
-
+import Image from "next/image";
 const ThemeToggle = () => {
   return (
     <div className={styles.container}>
-      ThemeToggle
+      <Image className={styles.moonTheme} src="/moon.png" alt="moon" width={14} height={14} />
+      <div className={styles.ball}></div>
+      <Image className={styles.sunTheme} src="/sun.png" alt="sun" width={14} height={14} />
     </div>
   )
 }
