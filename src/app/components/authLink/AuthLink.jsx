80ea47dde4c70408f60dda7c1ee868cb9923ac02
Ekpenisi Erue Raphael <ekpenisiraphael@gmail.com>
2023-11-13 10:33:38 +0530

    Active Mobile menu

--- src/app/components/authLink/AuthLink.jsx
+++ src/app/components/authLink/AuthLink.jsx
@@ -1,18 +1,42 @@
-import React from "react";
+"use client";
+import React, { useState, useEffect } from "react";
 import styles from "./authLink.module.css";
-import Link from "next/link"
+import Link from "next/link";
+
 const AuthLink = () => {
+  const [open, setOpen] = useState(false);
+
   const status = "notauth";
 
   return (
     <>
       {status === "notauth" ? (
-        <Link href="/login">Login</Link>
+        <Link href="/login"  className={styles.link}>Login</Link>
       ) : (
         <>
-          <Link href="/create">Create</Link>
-          <span>Logout</span>
+          <Link href="/create" className={styles.link}>Create</Link>
+          <span  className={styles.link}>Logout</span>
         </>
+      )}
+      <div className={styles.burger} onClick={() => setOpen(!open)}>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+      </div>
+      {open && (
+        <div className={styles.mobileMenu}>
+          <Link href="/">Home</Link>
+          <Link href="/">About</Link>
+          <Link href="/">Contact</Link>
+          {status === "notauth" ? (
+            <Link href="/login">Login</Link>
+          ) : (
+            <>
+              <Link href="/create">Create</Link>
+              <span>Logout</span>
+            </>
+          )}
+        </div>
       )}
     </>
   );
