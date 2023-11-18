8a9461c36cb471f0011c0f4680bcc66fd7488598
Ekpenisi Erue Raphael <ekpenisiraphael@gmail.com>
2023-11-18 15:02:36 +0530

    Home page design

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,14 +1,12 @@
 import Featured from "./components/Featured/featured";
 import Menu from "./components/Menu/Menu";
 import CardList from "./components/cardList/cardList";
-import CategoryList from "./components/categoryList/categoryList";
-import styles from "./homepage.module.css"
+import styles from "./homepage.module.css";
 
 export default function Home() {
   return (
   <div className={styles.container}>
      <Featured />
-<CategoryList />
 <div className={styles.content}>
   <CardList />
   <Menu />
