61313da611db32d5f69f9185e999aff29d638346
Ekpenisi Erue Raphael <ekpenisiraphael@gmail.com>
2023-11-18 15:01:01 +0530

    Footer design

--- src/app/components/Footer/Footer.jsx
+++ src/app/components/Footer/Footer.jsx
@@ -1,8 +1,84 @@
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 import styles from "./Footer.module.css";
 
 const Footer = () => {
-  return <div className={styles.container}>Footer</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.logoDiv}>
+        <Image className={styles.logo} src="/Srmlogo.jpg" alt="SRM" width={50} height={50} />
+        <h1 className={styles.pagename}>Sant Ralph Magazine</h1>
+      </div>
+      <p className={styles.details}>
+        Lorem ipsum eum neque fuga amet quo quas saepe ex voluptate esse vero, error sunt
+      </p>
+      <div className={styles.social}>
+        <Image src="/facebook.png" alt="Facebook" width={24} height={24} />
+        <Image src="/youtube.png" alt="Youtube" width={24} height={24} />
+        <Image src="/instagram.png" alt="Instagram" width={24} height={24} />
+        <Image src="/tiktok.png" alt="Tiktok" width={24} height={24} />
+        <Image src="/x.jpg" alt="Twitter" width={24} height={24} /> {/* Corrected Twitter image source */}
+      </div>
+      <div className={styles.footerlinks}>
+        <div>
+          <span className={styles.navtitle}>Title</span>
+          <div className={styles.links}>
+            <Link href="/" className={styles.link}>
+              Home
+            </Link>
+            <Link href="/about" className={styles.link}> 
+              About
+            </Link>
+            <Link href="/contact" className={styles.link}>
+              Contact
+            </Link>
+          </div>
+        </div>
+        <div className={styles.containercategories}>
+          <span className={styles.title}>Categories</span> {/* Corrected the closing tag */}
+          <div className={styles.categories}>
+            <Link
+              href="/blog?cat=liftStyle"
+              className={`${styles.category} ${styles.liftStyle}`}
+          >
+              LiftStyle
+            </Link>
+            <Link
+            href="/blog?cat=sport"
+            className={`${styles.category} ${styles.sport}`}
+          >
+           
+            Sport
+          </Link>
+          {/*  */}
+          <Link
+            href="/blog?cat=health"
+            className={`${styles.category} ${styles.health}`}
+          >
+           
+            Health
+          </Link>
+          {/*  */}
+          <Link
+            href="/blog?cat=technology"
+            className={`${styles.category} ${styles.technology}`}
+          >
+            
+            Technology
+          </Link>
+          {/*  */}
+          <Link
+            href="/blog?cat=treandingsNews"
+            className={`${styles.category} ${styles.treandingsNews}`}
+          >
+            TreandingsNews
+          </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
