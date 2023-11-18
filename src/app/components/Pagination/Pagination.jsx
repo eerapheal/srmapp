1885a95dc130ac9c7b8640e430d2b48d5b247e5c
Ekpenisi Erue Raphael <ekpenisiraphael@gmail.com>
2023-11-18 15:10:14 +0530

    pagenstion button design

--- src/app/components/Pagination/Pagination.jsx
+++ src/app/components/Pagination/Pagination.jsx
@@ -1,13 +1,14 @@
 import styles from "./Pagination.module.css";
 
-import React from 'react'
+import React from "react";
 
 const Pagination = () => {
   return (
     <div className={styles.container}>
-      Pagination
+      <button className={styles.left}>Previus</button>
+      <button className={styles.right}>Next</button>
     </div>
-  )
-}
+  );
+};
 
-export default Pagination
+export default Pagination;
