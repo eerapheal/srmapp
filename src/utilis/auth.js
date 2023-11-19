990ebd59b45e304d85a2c6acdb22738c32594a5c
Ekpenisi Erue Raphael <ekpenisiraphael@gmail.com>
2023-11-19 17:39:09 +0530

    Client adapter connection

--- src/utilis/auth.js
+++ src/utilis/auth.js
@@ -1,9 +1,8 @@
+import { PrismaAdapter } from "@auth/prisma-adapter"
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
-import { PrismaAdapter } from "@auth/prisma-adapter"
-import { PrismaClient } from "@prisma/client"
+import prisma from "./connect";
 
-const prisma = new PrismaClient()
 export const authOption = {
   adapter: PrismaAdapter(prisma),
   providers: [
