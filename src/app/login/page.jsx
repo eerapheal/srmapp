8d20da47163e052d9cbd3d7e200d57185d7e33ee
Ekpenisi Erue Raphael <ekpenisiraphael@gmail.com>
2023-11-19 14:12:47 +0530

    Login fuctionality and  Router navigation

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,11 +1,27 @@
+"use client"
+import { signIn, useSession } from "next-auth/react";
 import styles from "./Login.module.css";
+import { useRouter } from "next/navigation"
 
 const Login = () => {
   
+  const router = useRouter();
+
+  const {data,status} = useSession();
+ 
+  if(status ==="loading") {
+    return <div className={styles.loading}>Loading...</div>
+  }
+
+   if(status ==="authenticated") {
+    router.push("/")
+  }
+
+
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
-        <div className={styles.btn}>Continue with Google</div>
+        <div className={styles.btn} onClick={() => signIn("google")}>Continue with Google</div>
         <div className={styles.btn}>Continue with Facebook</div>
         <div className={styles.btn}>Continue with GitHub</div>
       </div>
