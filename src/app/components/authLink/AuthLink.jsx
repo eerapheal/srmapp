b8c3e8e4615e02e72385e095d5e821b16f4b9f12
Ekpenisi Erue Raphael <ekpenisiraphael@gmail.com>
2023-11-19 14:10:50 +0530

    Refactored and logout

--- src/app/components/authLink/AuthLink.jsx
+++ src/app/components/authLink/AuthLink.jsx
@@ -1,21 +1,22 @@
 "use client";
 import React, { useState, useEffect } from "react";
+import { signOut } from "next-auth/react"
 import styles from "./authLink.module.css";
 import Link from "next/link";
 
 const AuthLink = () => {
   const [open, setOpen] = useState(false);
 
-  const status = "notauth";
+  const status = "unauthenticated";
 
   return (
     <>
-      {status === "notauth" ? (
+      {status === "notauthenticated" ? (
         <Link href="/login"  className={styles.link}>Login</Link>
       ) : (
         <>
           <Link href="/create" className={styles.link}>Create</Link>
-          <span  className={styles.link}>Logout</span>
+          <span onClick={signOut} className={styles.link}>Logout</span>
         </>
       )}
       <div className={styles.burger} onClick={() => setOpen(!open)}>
@@ -28,12 +29,12 @@ const AuthLink = () => {
           <Link href="/">Home</Link>
           <Link href="/">About</Link>
           <Link href="/">Contact</Link>
-          {status === "notauth" ? (
+          {status === "notauthenticated" ? (
             <Link href="/login">Login</Link>
           ) : (
             <>
               <Link href="/create">Create</Link>
-              <span>Logout</span>
+              <span onClick={signOut}>Logout</span>
             </>
           )}
         </div>
