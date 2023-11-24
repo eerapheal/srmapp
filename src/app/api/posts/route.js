ebc4bf43068b94ec4407cfb42ca86268258763d9
Ekpenisi Erue Raphael <ekpenisiraphael@gmail.com>
2023-11-24 13:49:33 +0530

    post qurry

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -7,15 +7,19 @@ export const GET = async (req) => { // Added 'req' parameter
   const page = parseInt(searchParams.get("page")) || 1; // Parse the page parameter as an integer
   const POST_PER_PAGE = 2;
 
+  const query = {
+    take: POST_PER_PAGE,
+    skip: POST_PER_PAGE * (page -1),
+  }
   try {
-    const posts = await prisma.post.findMany({
-      take: POST_PER_PAGE,
-      skip: POST_PER_PAGE * (page - 1),
-    });
+    const [posts, count] = await prisma.$transaction([
+      prisma.post.findMany(query),
+      prisma.post.count(),
+    ]);
 
-    return new NextResponse(JSON.stringify(posts, { status: 200 }));
+    return new NextResponse(JSON.stringify({posts, count}, { status: 200 }));
   } catch (err) {
-    console.error(err); // Use console.error for logging errors
+    console.error(err); 
     return new NextResponse(
       JSON.stringify({ message: "Something went wrong" }, { status: 500 })
     );
