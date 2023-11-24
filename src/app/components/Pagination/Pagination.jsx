e6ebfd2a09ebf152cb76e5ff1b41d16c59ff272f
Ekpenisi Erue Raphael <ekpenisiraphael@gmail.com>
2023-11-24 13:50:46 +0530

    post pagination

--- src/app/components/Pagination/Pagination.jsx
+++ src/app/components/Pagination/Pagination.jsx
@@ -1,14 +1,27 @@
+"use client"
+
 import styles from "./Pagination.module.css";
-
 import React from "react";
+import { useRouter } from "next/navigation";
 
-const Pagination = () => {
+const Pagination = ({page, hasPrev, hasNext}) => {
+  const rooter  = useRouter()    
   return (
     <div className={styles.container}>
-      <button className={styles.left}>Previus</button>
-      <button className={styles.right}>Next</button>
+      <button className={styles.left}
+      disabled={!hasPrev}
+      onClick={() => rooter.push('?page=${page -1} ')}
+      >
+        Previous
+        </button>
+      <button className={styles.right}
+       disabled={!hasNext}
+       onClick={() => rooter.push('?page=${page +1} ')}
+      >
+        Next
+        </button>
     </div>
-  );
+  ); 
 };
 
 export default Pagination;
