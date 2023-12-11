3a1637dfd7116f569d965bb6a18f910493e472de
Ekpenisi Erue Raphael <ekpenisiraphael@gmail.com>
2023-12-11 05:39:13 +0530

    Blog category pages

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -2,12 +2,15 @@ import styles from "./BlogPage.module.css";
 import CardList from "../components/cardList/CardList";
 import Menu from "../components/Menu/Menu";
 
-const BlogPage = () => {
+const BlogPage = ({ searchParams }) => {
+  const page = parseInt(searchParams.page) || 1;
+  const { cat } = searchParams;
+
   return (
-    <div className={styles.contanier}>
-      <h1 className={styles.title}>All Sport</h1>
-      <div className={styles.SportItems}>
-        <CardList />
+    <div className={styles.container}>
+      <h1 className={styles.title}>{cat} Blog</h1>
+      <div className={styles.content}>
+        <CardList page={page} cat={cat}/>
         <Menu />
       </div>
     </div>
