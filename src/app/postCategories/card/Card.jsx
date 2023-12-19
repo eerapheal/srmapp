072d74df5bc3598d4d6e167c93435a862aefb66b
Ekpenisi Erue Raphael <ekpenisiraphael@gmail.com>
2023-12-19 23:00:21 +0530

    Bug fix

--- src/app/postCategories/card/Card.jsx
+++ src/app/postCategories/card/Card.jsx
@@ -3,29 +3,26 @@ import styles from "./card.module.css";
 import Image from "next/image";
 import Link from "next/link";
 
-
-const Card = ({ key, item }) => {
+const Card = ({ item }) => {
   return (
-    <div className={styles.container} key={key}>
-  <div className={styles.card}>
-  <Link href={`/posts/${item.slug}`} alt="items" className={styles.item}>
-  {item.image && (
-        <div  className={styles.imgContainer}>
-
-          <Image src={item.image} alt="" fill className={styles.image} />
-        </div>
-      )}
-      <div className={styles.textContainer}>
-        <div className={styles.detail}>
-          <span className={styles.date}>
-            {item.createdAt.substring(0, 10)} {" "}
-          </span>
-          {/* <span className={styles.category}>{item.catSlug}</span> */}
-        </div>
-        <div className={styles.desc} dangerouslySetInnerHTML={{ __html: item?.desc.substring(0, 139) }}/>
+    <div className={styles.container}>
+      <div className={styles.card}>
+        <Link href={`/posts/${item.slug}`} alt="items" className={styles.item}>
+          {item.image && (
+            <div className={styles.imgContainer}>
+              <Image src={item.image} alt="" fill className={styles.image} />
+            </div>
+          )}
+          <div className={styles.textContainer}>
+            <div className={styles.detail}>
+              <span className={styles.date}>
+                {item.createdAt.substring(0, 10)} {" "}
+              </span>
+            </div>
+            <div className={styles.desc} dangerouslySetInnerHTML={{ __html: item?.desc.substring(0, 139) }} />
+          </div>
+        </Link>
       </div>
-      </Link>
-  </div>
     </div>
   );
 };
