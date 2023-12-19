54f3ccb2df0e2b707f95c9711d3ff5b3f38d3b8e
Ekpenisi Erue Raphael <ekpenisiraphael@gmail.com>
2023-12-19 22:33:59 +0530

    Card adjustments

--- src/app/components/card/Card.jsx
+++ src/app/components/card/Card.jsx
@@ -3,32 +3,31 @@ import styles from "./card.module.css";
 import Image from "next/image";
 import Link from "next/link";
 
-
-const Card = ({ key, item }) => {
+const Card = ({ item }) => {
   return (
-    <div className={styles.container} key={key}>
-  <div className={styles.card}>
-  {item.image && (
-        <div className={styles.postImg}>
-          <Image src={item.image} alt="" fill className={styles.image} />
+    <div className={styles.container}>
+      <div className={styles.card}>
+        {item.image && (
+          <div className={styles.postImg}>
+            <Image src={item.image} alt="" fill className={styles.image} />
+          </div>
+        )}
+        <div className={styles.content}>
+          <div className={styles.detail}>
+            <span className={styles.date}>
+              {item.createdAt.substring(0, 10)} {" "}
+            </span>
+            <span className={styles.category}>{item.catSlug}</span>
+          </div>
+          <Link href={`/posts/${item.slug}`}>
+            <h1 className={styles.title}>{item.title}</h1>
+          </Link>
+          <div className={styles.desc} dangerouslySetInnerHTML={{ __html: item?.desc.substring(0, 139) }} />
+          <Link href={`/posts/${item.slug}`} className={styles.action}>
+            Read More
+          </Link>
         </div>
-      )}
-      <div className={styles.content}>
-        <div className={styles.detail}>
-          <span className={styles.date}>
-            {item.createdAt.substring(0, 10)} {" "}
-          </span>
-          <span className={styles.category}>{item.catSlug}</span>
-        </div>
-        <Link href={`/posts/${item.slug}`}>
-          <h1 className={styles.title}>{item.title}</h1>
-        </Link>
-        <div className={styles.desc} dangerouslySetInnerHTML={{ __html: item?.desc.substring(0, 139) }}/>
-        <Link href={`/posts/${item.slug}`} className={styles.action}>
-          Read More
-        </Link>
       </div>
-  </div>
     </div>
   );
 };
