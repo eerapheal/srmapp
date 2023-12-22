d567cf12948069c08fc940aa1083a50e0f16dcf1
Ekpenisi Erue Raphael <ekpenisiraphael@gmail.com>
2023-12-22 19:58:35 +0530

    Card file location change

--- src/app/cardList/cardList.jsx
+++ src/app/cardList/cardList.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./cardList.module.css";
 import Pagination from "../pagination/pagePage";
-import Card from "../../components/card/postCard";
+import Card from "../card/postCard";
 
 const getData = async (page, cat) => {
   const res = await fetch(
