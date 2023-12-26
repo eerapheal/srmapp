8d4249d44646f87a0d109cd95e1693ccb869584f
Ekpenisi Erue Raphael <ekpenisiraphael@gmail.com>
2023-12-27 03:17:59 +0530

    local host

--- next.config.js
+++ next.config.js
@@ -3,9 +3,6 @@ const nextConfig = {
   images: {
     domains: ["lh3.googleusercontent.com", "firebasestorage.googleapis.com"],
   },
-  env: {
-    API_URL: 'http://127.0.0.1:3000',
-  },
 }
 
 module.exports = nextConfig;
