7c2034fa3c0bf75f5c90767b3ef691a099e984bd
Ekpenisi Erue Raphael <ekpenisiraphael@gmail.com>
2023-12-26 18:52:48 +0530

    local host

--- next.config.js
+++ next.config.js
@@ -4,10 +4,8 @@ const nextConfig = {
     domains: ["lh3.googleusercontent.com", "firebasestorage.googleapis.com"],
   },
   env: {
-    API_URL: process.env.NODE_ENV === 'production'
-      ? 'https://srmagazine.vercel.app'
-      : 'http://127.0.0.1:3000',
+    API_URL: 'https://srmagazine.vercel.app',
   },
-};
+}
 
 module.exports = nextConfig;
