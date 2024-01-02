3e57f53530a7e6864e5b4c2b069bb1373503c613
Ekpenisi Erue Raphael <ekpenisiraphael@gmail.com>
2024-01-03 03:59:33 +0530

    BUG FIX

--- src/components/authLink/AuthLink.jsx
+++ src/components/authLink/AuthLink.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { signOut, useSession } from "next-auth/react";
 import styles from "./authLink.module.css";
 import Link from "next/link";
@@ -35,7 +35,7 @@ const AuthLink = () => {
           <Link href="/">Home</Link>
           <Link href="/">About</Link>
           <Link href="/">Contact</Link>
-          {status === "notauthenticated" ? (
+          {status === "unauthenticated" ? (
             <Link href={`/login`}>Login</Link>
           ) : (
             <>
