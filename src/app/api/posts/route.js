7f7ea5a11467b496e1215204a84f7ba877a78253
Ekpenisi Erue Raphael <ekpenisiraphael@gmail.com>
2024-01-03 03:57:57 +0530

    Adjustment

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -8,7 +8,7 @@ export const GET = async (req) => {
   const page = searchParams.get("page");
   const cat = searchParams.get("cat");
 
-  const POST_PER_PAGE = 20;
+  const POST_PER_PAGE = 16;
 
   const query = {
     take: POST_PER_PAGE,
