d2f017d77014075fb177ca145c4858de2d140f55
Ekpenisi Erue Raphael <ekpenisiraphael@gmail.com>
2024-01-03 00:26:35 +0530

    Modified

--- src/app/api/popular/route.js
+++ src/app/api/popular/route.js
@@ -1,17 +1,34 @@
 import prisma from "@/utilis/connect";
 import { NextResponse } from "next/server";
 
-export const GET = async (req, res) => {
+export  const GET = async (req) => {
+  if (req.method !== "GET") {
+    return new NextResponse(
+      JSON.stringify({ message: "Method Not Allowed" }, { status: 405 })
+    );
+  }
+
+  const { categoryName } = req.query;
+
   try {
-    const popularPosts = await prisma.post.findMany({
-      orderBy: { views: "desc" },
-      take: 5, // Change this number based on how many popular posts you want to retrieve
-      include: { user: true },
+    const category = await prisma.category.findUnique({
+      where: { name: categoryName },
+      select: {
+        features: true,
+      },
     });
 
-    return new NextResponse(JSON.stringify(popularPosts), { status: 200 });
+    if (!category) {
+      return new NextResponse(
+        JSON.stringify({ message: "Category not found!" }, { status: 404 })
+      );
+    }
+
+    return new NextResponse(JSON.stringify(category.features, { status: 200 }));
   } catch (err) {
     console.error(err);
-    return new NextResponse(JSON.stringify({ message: "Something went wrong!" }), { status: 500 });
+    return new NextResponse(
+      JSON.stringify({ message: "Something went wrong!" }, { status: 500 })
+    );
   }
 };
