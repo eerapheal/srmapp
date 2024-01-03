93b865180b3af2712abfbac269253776b0f55961
Ekpenisi Erue Raphael <ekpenisiraphael@gmail.com>
2024-01-04 03:54:06 +0530

    Icon and links

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,14 +1,21 @@
 const About = () => {
-    return (
-        <div>
-            <h1 >Sant Ralph Magazine</h1>
-      <p >
-        Explore the latest trends in sports, health, technology, lifestyle, and
-        entertainment. Stay informed and entertained with our diverse range of
-        articles on cutting-edge topics.
+  return (
+    <div>
+      <h1>Sant Ralph Magazine</h1>
+      <p>
+        Sant Ralph Magazine is your one-stop destination for the latest trends
+        in sports, health, lifestyle, entertainment, technology, and history.
+        Our diverse range of articles ensures you stay informed and entertained.
+        From in-depth coverage of sports highlights, breakthroughs in health and
+        wellness, and lifestyle trends to the hottest entertainment releases and
+        the latest in technology, we've got it all. Dive into captivating
+        stories that bridge the past and present, making history accessible and
+        engaging. Sant Ralph Magazine is not just a publication; it's your
+        passport to a world of dynamic and enriching content. Stay in the know
+        with us.
       </p>
-        </div>
-    )
-}
+    </div>
+  );
+};
 
 export default About;
--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -5,18 +5,43 @@ const Contact = () => {
     <div>
       <div>
         <h1>Sant Ralph Magazine</h1>
-        <p >
-          Explore the latest trends in sports, health, technology, lifestyle,
-          and entertainment. Stay informed and entertained with our diverse
-          range of articles on cutting-edge topics.
+        <p>
+          Embark on an enriching journey with Sant Ralph Magazine, your go-to
+          source for the latest trends in sports, health, technology, lifestyle,
+          and entertainment. Curated by Ekpenisi Erue Raphael, our platform
+          offers a harmonious blend of informative and captivating content.
+          Explore sports victories, wellness tips, and the latest tech
+          innovations. Immerse yourself in lifestyle trends, and celebrate the
+          vibrancy of entertainment in film, music, literature, and art. Managed
+          by Raphael, Sant Ralph Magazine goes beyond content delivery; it
+          crafts an immersive experience. Advertisers can leverage prime
+          opportunities to authentically connect with their audience, and our
+          services extend to website development, graphic design, video editing,
+          digital marketing, SEO optimization, and more. Join us in this dynamic
+          medium where every page unfolds a new adventure. Sant Ralph Magazine
+          beckons, inviting you to explore the boundless horizons of knowledge,
+          innovation, and entertainment.
         </p>
       </div>
-      <div >
-        <Image src="/facebook.png" alt="Facebook" width={24} height={24} />
-        <Image src="/youtube.png" alt="LinkedIn" width={24} height={24} />
-        <Image src="/instagram.png" alt="Email" width={24} height={24} />
-        <Image src="/tiktok.png" alt="Facebook" width={24} height={24} />
-        <Image src="/x.jpg" alt="Twitter" width={24} height={24} />
+      <div>
+        <Link href="mailto:ekpenisiraphael@gmail.com">
+          <Image src="/email.jpeg" alt="Email" width={24} height={24} />
+        </Link>
+        <Link href="tel:+918826120974">
+          <Image src="/tell.jpeg" alt="Phone" width={24} height={24} />
+        </Link>
+        <Link
+          href="https://www.linkedin.com/in/ekpenisiraphael"
+          target="_blank"
+        >
+          <Image src="/linkedin.jpeg" alt="LinkedIn" width={24} height={24} />
+        </Link>
+        <Link href="https://github.com/eerapheal" target="_blank">
+          <Image src="/gitHub.jpeg" alt="GitHub" width={24} height={24} />
+        </Link>
+        <Link href="https://twitter.com/ekpenisiraphael" target="_blank">
+          <Image src="/x.jpeg" alt="Twitter" width={24} height={24} />
+        </Link>
       </div>
     </div>
   );
