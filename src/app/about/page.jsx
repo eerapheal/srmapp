0efc7007ba8045fd2a69404886cf3848a46c0bcf
Ekpenisi Erue Raphael <ekpenisiraphael@gmail.com>
2024-01-03 05:41:08 +0530

    About page add

---
+++ src/app/about/page.jsx
@@ -0,0 +1,14 @@
+const About = () => {
+    return (
+        <div>
+            <h1 >Sant Ralph Magazine</h1>
+      <p >
+        Explore the latest trends in sports, health, technology, lifestyle, and
+        entertainment. Stay informed and entertained with our diverse range of
+        articles on cutting-edge topics.
+      </p>
+        </div>
+    )
+}
+
+export default About;
