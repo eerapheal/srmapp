fa12745b0faafff1ab2a0baee3a3e8cf3efceba2
Ekpenisi Erue Raphael <ekpenisiraphael@gmail.com>
2024-01-04 05:19:42 +0530

    Privacy Policy

---
+++ src/app/privacy/page.jsx
@@ -0,0 +1,94 @@
+const Privacy = () => {
+  return (
+    <div style={{ margin: "40px" }}>
+      <h1 style={{  padding: "50px 0", color: "blue" , textAlign: "center" }}>Privacy Policy</h1>
+
+      <p>
+        Welcome to Sant Ralph Magazine! At Sant Ralph Magazine, we prioritize
+        the privacy and security of your personal information. This Privacy
+        Policy outlines how we collect, use, and protect your data. By using our
+        website, you agree to the terms presented in this policy.
+      </p>
+
+      <h2 style={{ color: "green" , padding: "8px" }}>1. Information We Collect</h2>
+
+      <p>
+        <strong>1.1 Personal Information:</strong> We may collect personal
+        information, such as your contact details (name, email address),
+        demographic information, and user preferences.
+      </p>
+
+      <p>
+        <strong>1.2 Usage Data and Cookies:</strong> Non-personal information is
+        collected through cookies to enhance your browsing experience, analyze
+        site traffic, and improve our services. Refer to our{" "}
+        <a href="#cookie-policy">Cookie Policy</a> for detailed information on
+        the types of cookies used.
+      </p>
+
+      <h2 style={{ color: "green" , padding: "8px" }}>2. How We Use Your Information</h2>
+
+      <p>
+        We use your information for purposes including personalization, content
+        improvement, communication of updates, newsletters, and promotional
+        offers, and to comply with legal obligations.
+      </p>
+
+      <h2 style={{ color: "green", padding: "8px" }}>3. Data Security</h2>
+
+      <p>
+        We implement security measures to protect your personal information from
+        unauthorized access, disclosure, alteration, and destruction. While we
+        strive for security, no method of transmission or storage can guarantee
+        100% security.
+      </p>
+
+      <h2 style={{ color: "green", padding: "8px" }}>4. Consent and Control</h2>
+
+      <p>
+        <strong>4.1 Consent:</strong> By using Sant Ralph Magazine, you consent
+        to the collection, processing, and storage of your personal information
+        as outlined in this Privacy Policy.
+      </p>
+
+      <p>
+        <strong>4.2 User Control:</strong> You have the right to access, update,
+        or delete your personal information. Manage your cookie preferences
+        through your browser settings.
+      </p>
+
+      <h2 style={{ color: "green", padding: "8px" }}>5. GDPR and CPRA Compliance</h2>
+
+      <p>
+        <strong>5.1 Transparency:</strong> We are committed to transparency in
+        the processing of personal data. This policy provides detailed
+        information on how we handle your information.
+      </p>
+
+      <p>
+        <strong>5.2 User Rights:</strong> You have the right to request access,
+        rectification, erasure, and restriction of processing. For inquiries,
+        contact us at{" "}
+        <a href="mailto:support@srmspot.com">support@srmspot.com</a>.
+      </p>
+
+      <h2 style={{ color: "green", padding: "8px" }}>6. Changes to Privacy Policy</h2>
+
+      <p>
+        We reserve the right to update this Privacy Policy to reflect changes in
+        our practices and legal requirements. Material changes will be
+        communicated by posting the updated policy on our website.
+      </p>
+
+      <h2 style={{ color: "green", padding: "8px" }}>7. Contact Us</h2>
+
+      <p>
+        If you have any questions or concerns regarding this Privacy Policy,
+        please contact us at{" "}
+        <a href="mailto:support@srmspot.com">support@srmspot.com</a>.
+      </p>
+    </div>
+  );
+};
+
+export default Privacy;
